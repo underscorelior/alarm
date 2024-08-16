082b9a70d4cfc52cd893bb57b2becc59405293b9
underscorelior <newlioryt@gmail.com>
2024-08-15 20:41:48 -0700

    More menu work

--- main.py
+++ main.py
@@ -3,109 +3,156 @@ from time import sleep
 from pico_i2c_lcd import I2cLcd
 import ds1307
 import tm1637
-from menu import MainMenu, SettingsMenu
-
+from menu import MainMenu, SettingsMenu, SkipMenu
 
 #### SETUP DEVICES ####
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 lcd = I2cLcd(i2c, 0x27, 2, 16)
 
-i2c1 = I2C(1, sda=Pin(10), scl=Pin(11), freq=400000)
+i2c1 = I2C(1, sda=Pin(14), scl=Pin(15), freq=400000)
 rtc = ds1307.DS1307(i2c1, 0x68)
 
 sevseg = tm1637.TM1637(clk=machine.Pin(5), dio=machine.Pin(4))
 
-buzzer = PWM(Pin(15))
+buzzer = PWM(Pin(17))
 
 joystick = (ADC(Pin(27)), ADC(Pin(26)), Pin(16, Pin.IN, Pin.PULL_UP))
 
-lcd.custom_char(0, bytearray([ 
- 0x00,
-  0x08,
-  0x0C,
-  0x0E,
-  0x0E,
-  0x0C,
-  0x08,
-  0x00]))
+lcd.custom_char(0, bytearray([0x00, 0x08, 0x0C, 0x0E, 0x0E, 0x0C, 0x08, 0x00]))
 
-#### 7SEG DISPLAY + ALARM #### 
-def display_time(dt, colon = False):
+#### KEYPAD ####
+col_list = [6, 7, 8, 9]
+row_list = [10, 11, 12, 13]
+
+for x in range(0, 4):
+    row_list[x] = Pin(row_list[x], Pin.OUT)
+    row_list[x].value(1)
+
+for x in range(0, 4):
+    col_list[x] = Pin(col_list[x], Pin.IN, Pin.PULL_UP)
+
+
+key_map = [
+    ["D", "#", "0", "*"],
+    ["C", "9", "8", "7"],
+    ["B", "6", "5", "4"],
+    ["A", "3", "2", "1"],
+]
+
+
+def Keypad4x4Read(cols, rows):
+    for r in rows:
+        r.value(0)
+        result = [cols[0].value(), cols[1].value(), cols[2].value(), cols[3].value()]
+        if min(result) == 0:
+            key = key_map[int(rows.index(r))][int(result.index(0))]
+            r.value(1)  # manages key keept pressed
+            return key
+        r.value(1)
+
+
+#### 7SEG DISPLAY + ALARM ####
+def display_time(dt, colon=False):
     if colon:
-        sevseg.numbers(dt[4],dt[5])
+        sevseg.numbers(dt[4], dt[5])
     else:
         sevseg.show(f"{dt[4]:02d}{dt[5]:02d}")
+
 
 def manage_alarm(dt):
     pass
 
-#### JOYSTICK #### 
+
+#### JOYSTICK ####
 CENTER = 32759
-def handle_horiz(): # -1 = center, 0 = left, 1 = right
+
+
+def handle_horiz():  # -1 = center, 0 = left, 1 = right
     value = joystick[0].read_u16()
 
-    if value < (CENTER * 2 / 3): # If is lesser than 2/3 (left)
+    if value < (CENTER * 2 / 3):  # If is lesser than 2/3 (left)
         return 0
-    elif value < (CENTER * 4 / 3): # If is greater than 4/3 (right)
+    elif value < (CENTER * 4 / 3):  # If is greater than 4/3 (right)
         return -1
-    else: # Otherwise (center)
-        return 1 
+    else:  # Otherwise (center)
+        return 1
 
-def handle_vert(): # -1 = center, 1 = down, 0 = up
+
+def handle_vert():  # -1 = center, 1 = down, 0 = up
     value = joystick[1].read_u16()
 
-    if value < (CENTER * 2 / 3): 
+    if value < (CENTER * 2 / 3):
         return 1
     elif value < (CENTER * 4 / 3):
         return -1
     else:
         return 0
 
-def handle_press(): # 0 = not pressed, 1 = pressed
+
+def handle_press():  # 0 = not pressed, 1 = pressed
     if joystick[2].value() == 1:
         return 0
     else:
         return 1
 
 
+mainmenu = MainMenu(
+    lcd,
+    {"Skip": SkipMenu(lcd), "Settings": SettingsMenu(lcd), "Alarm": SettingsMenu(lcd)},
+)
 #### MAIN LOOP ####
 rerender = False
 colon = 0
-menu = MainMenu(lcd, {'Skip': SettingsMenu(lcd), 'Settings': SettingsMenu(lcd), 'Alarm': SettingsMenu(lcd)})
+skip_days = []
+menu = mainmenu
 
 menu.draw_menu()
+
+
+def do_menu(dt):
+    if type(menu) is SkipMenu:
+        menu.set_skips(skip_days, f"{dt[0]}{dt[1]}{dt[2]+1}")
+
+    menu.reset()
+    menu = mainmenu
+    menu.draw_menu()
+
 
 while True:
     dt = rtc.datetimeRTC
     display_time(dt, colon > 1)
     rerender = menu.handle_joystick(handle_horiz(), handle_vert())
-    new = menu.handle_press(handle_press())
+    new = menu.handle_press(handle_press(), mainmenu)
 
-    if new:
-        menu = new
+    key = Keypad4x4Read(col_list, row_list)
 
-        menu.draw_menu()
-    
+    if key != None:
+        print("Pressed button: " + key)
+
+    if new is True:
+        skip_days.append(f"{dt[0]}{dt[1]}{dt[2]+1}")
+        do_menu(dt)
+
+    elif new:
+        do_menu(dt)
+
     if rerender:
         menu.draw_menu()
-    
+
     sleep(0.5)
     colon = (colon + 1) % 4
     rerender = False
 
-
-
-
-#### IDEAS ####
-# Menu:
-## Change -> Used for changing time
+    #### IDEAS ####
+    # Menu:
+    ## Change -> Used for changing time
     ## Shows up on the 4 digit maybe, and if you go vertically with joystick it allows you to change it up or down.
     ## If you go horiz it changes HH to MM and vice versa.
     ## Indicates which is used by blinking the one that is being edited
     ## Asks if it is for one day or a permanent change
-## Skip -> Skips next day
+    ## Skip -> Skips next day
     ## Asks for confirmation
-## Settings:
+    ## Settings:
     ## Allows you to change the variables related to the game
     ## Change the volume of the alarm clock, with a preview sound playing
     ## Change the days it is active on, like below
@@ -116,9 +163,9 @@ while True:
 
 # Game:
 ## Picks between 3 games randomly:
-    ## Math:
-        ## Puts a customizable amount of math problems to be solved.
-    ## Capitals:
-        ## Guess country capital with the abcd of the keypad.
-    ## Memory:
-        ## Shows arrows on the LCD, you are required to memorize and do with joystick.
+## Math:
+## Puts a customizable amount of math problems to be solved.
+## Capitals:
+## Guess country capital with the abcd of the keypad.
+## Memory:
+## Shows arrows on the LCD, you are required to memorize and do with joystick.
--- menu.py
+++ menu.py
@@ -1,9 +1,11 @@
-def sel_chr(num, options, sel, char = chr(0), other = " "):
+def sel_chr(num, options, sel, char=chr(0), other=" "):
     return (char if sel == num else other) + options[num]
 
-def space_between(str1, str2, spacer = " ", length = 16):
-    spacing = spacer * (length - (len(str1+str2))) 
+
+def space_between(str1, str2, spacer=" ", length=16):
+    spacing = spacer * (length - (len(str1 + str2)))
     return str1 + spacing + str2
+
 
 # Options should be a dict, where key is the text to display and value is the Menu object for the next
 class MainMenu:
@@ -11,8 +13,10 @@ class MainMenu:
         self.display = display
         self.options = options
         self.selected = 0
-        # self. 
-    
+
+    def reset(self):
+        self.selected = 0
+
     def draw_menu(self):
         opts = list(self.options.keys())
         sel = self.selected
@@ -21,7 +25,7 @@ class MainMenu:
         lcd.clear()
         lcd.putstr(space_between(sel_chr(0, opts, sel), sel_chr(1, opts, sel)))
         lcd.putstr(sel_chr(2, opts, sel))
-        
+
     def handle_joystick(self, horizontal, vertical):
         sel = self.selected
 
@@ -30,30 +34,78 @@ class MainMenu:
         if horizontal != -1:
             if sel // 2 == 0:
                 sel = horizontal
-            # else: 
+                # else:
                 # sel = horizontal+2 # There is no 4th option ATM, so if this case is met, do nothing
 
                 rerender = True
-        
+
         if vertical != -1:
-            if sel % 2 == 0: 
-                sel = vertical*2
-            # else: 
+            if sel % 2 == 0:
+                sel = vertical * 2
+                # else:
                 # sel = vertical*2+1 # There is no 4th option ATM, so if this case is met, do nothing
                 rerender = True
 
         if sel == self.selected:
             rerender = False
-        
+
         self.selected = sel
 
         return rerender
 
-    def handle_press(self, press):
-        print("A")
+    def handle_press(self, press, _):
         if press:
             return self.options[list(self.options.keys())[self.selected]]
-        return False 
+        return False
+
+
+class SkipMenu:
+    def __init__(self, display):
+        self.display = display
+        self.selected = 0
+        self.options = ["Confirm", "Back"]
+        self.skips = []
+        self.date = ""
+
+    def reset(self):
+        self.selected = 0
+
+    def set_skips(self, skip_days):
+        self.skips = skip_days
+        self.skips
+
+    def draw_menu(self):
+        lcd = self.display
+        sel = self.selected
+        opt = self.options
+
+        lcd.clear()
+        lcd.putstr("Skip Next Alarm".center(16))
+        lcd.putstr(sel_chr(0, opt, sel) + " " * 3 + sel_chr(1, opt, sel))
+
+    def handle_joystick(self, horizontal, vertical):
+        sel = self.selected
+
+        rerender = False
+
+        if horizontal != -1:
+            sel = (sel - 1 + (horizontal * 2)) % 2
+            rerender = True
+
+        if sel == self.selected:
+            rerender = False
+
+        self.selected = sel
+
+        return rerender
+
+    def handle_press(self, press, main):
+        if press and self.selected == 1:
+            return main
+        if press and self.selected == 0:
+            return True
+        return False
+
 
 class SettingsMenu:
     def __init__(self, display):
@@ -74,21 +126,21 @@ class SettingsMenu:
         if horizontal != -1:
             if sel // 2 == 0:
                 sel = horizontal
-            # else: 
+                # else:
                 # sel = horizontal+2 # There is no 4th option ATM, so if this case is met, do nothing
 
                 rerender = True
-        
+
         if vertical != -1:
-            if sel % 2 == 0: 
-                sel = vertical*2
-            # else: 
+            if sel % 2 == 0:
+                sel = vertical * 2
+                # else:
                 # sel = vertical*2+1 # There is no 4th option ATM, so if this case is met, do nothing
                 rerender = True
 
         if sel == self.selected:
             rerender = False
-        
+
         self.selected = sel
 
         return rerender
